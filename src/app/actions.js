031ea83caae6974d348fcb73371df06f2e82c115
Peter Shafer <peter.shafer@gmail.com>
2017-05-01 18:41:04 -0700

    [#9] Unifying tab styling

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,24 +1,23 @@
-// A sample actions file.
 var actions = (function(){
-    var API = {
-        'loadDatabase': plux.createAction("loadDatabase"),
-        'restoreDatabase': plux.createAction("restoreDatabase"),
-        'loadComplete': plux.createAction("loadComplete"),
-        'getUnreadCount': plux.createAction("getUnreadCount"),
-        'getSection': plux.createAction("getSection"),
-        'readItem': plux.createAction("readItem"),
-        'getItem': plux.createAction("getItem"),
-        'markSectionRead': plux.createAction("markSectionRead"),
-        'openAddForm': plux.createAction("openAddForm"),
-        'addItem': plux.createAction("addItem"),
-        'nextItem': plux.createAction("nextItem"),
-        'nextUnread': plux.createAction("nextUnread"),
-        'updateFilter': plux.createAction("updateFilter"),
-        'bookmark': plux.createAction("bookmark"),
-        'debugReset': plux.createAction("debugReset"),
-        'debugAddAll': plux.createAction("debugAddAll"),
-        'debugAddSection': plux.createAction("debugAddSection"),
-        'debugAddRandom': plux.createAction("debugAddRandom")
-    };
-    return API;
+  var API = {
+    'loadDatabase': plux.createAction("loadDatabase"),
+    'restoreDatabase': plux.createAction("restoreDatabase"),
+    'loadComplete': plux.createAction("loadComplete"),
+    'getUnreadCount': plux.createAction("getUnreadCount"),
+    'getSection': plux.createAction("getSection"),
+    'readItem': plux.createAction("readItem"),
+    'getItem': plux.createAction("getItem"),
+    'markSectionRead': plux.createAction("markSectionRead"),
+    'openAddForm': plux.createAction("openAddForm"),
+    'addItem': plux.createAction("addItem"),
+    'nextItem': plux.createAction("nextItem"),
+    'nextUnread': plux.createAction("nextUnread"),
+    'updateFilter': plux.createAction("updateFilter"),
+    'bookmark': plux.createAction("bookmark"),
+    'debugReset': plux.createAction("debugReset"),
+    'debugAddAll': plux.createAction("debugAddAll"),
+    'debugAddSection': plux.createAction("debugAddSection"),
+    'debugAddRandom': plux.createAction("debugAddRandom")
+  };
+  return API;
 })();
--- src/app/store.js
+++ src/app/store.js
@@ -1,105 +1,105 @@
 // A sample store file.
 (function(){
-    var myActionHandler = function(action, data, state){
-        switch(action){
-            case "loadDatabase":
-                state.loadingDB = true;
-                var p1 = fetch('/src/app/database.json');
-                var p2 = fetch('/src/app/prettycontent.json');
-                Promise.all([p1, p2]).then(function(values) {
-                    var p1 = values[0].json();
-                    var p2 = values[1].json();
-                    Promise.all([p1, p2]).then(function(objs){
-                        state.database = objs[0];
-                        state.content = objs[1];
-                        actions.loadComplete();
-                    });
-                });
-                break;
-            case "restoreDatabase":
-                state.database = data.database;
-                state.content = data.content;
-                state.loadingDB = false;
-                state.databaseReady = true;
+  var myActionHandler = function(action, data, state){
+    switch(action){
+      case "loadDatabase":
+        state.loadingDB = true;
+        var p1 = fetch('/src/app/database.json');
+        var p2 = fetch('/src/app/prettycontent.json');
+        Promise.all([p1, p2]).then(function(values) {
+            var p1 = values[0].json();
+            var p2 = values[1].json();
+            Promise.all([p1, p2]).then(function(objs){
+                state.database = objs[0];
+                state.content = objs[1];
                 actions.loadComplete();
-                break;
-            case "loadComplete":
-                state.loadingDB = false;
-                state.databaseReady = true;
-                break;
-            case "readItem":
-                var code = data;
-                for(var i = 0; i < state.database.content.sections.length; i++){
-                    for(var j = 0; j < state.database.content.sections[i].items.length; j++){
-                        if(state.database.content.sections[i].items[j].id == code){
-                            if(!state.database.content.sections[i].items[j].read){
-                                state.database.content.sections[i].unread--; 
-                            }
-                            state.database.content.sections[i].items[j].read = true;
-                        }
-                    }
-                }
-                break;
-            case "markSectionRead":
-                var code = data;
-                for(var i = 0; i < state.database.content.sections.length; i++){
-                    if(state.database.content.sections[i].id == code){
-                        for(var j = 0; j < state.database.content.sections[i].items.length; j++){
-                            state.database.content.sections[i].items[j].read = true;
-                            state.database.content.sections[i].unread--;
-                        }
-                    }
-                }
-                break;
-            case "addItem":
-                var code = data;
-                for(var i = 0; i < state.database.content.sections.length; i++){
-                    for(var j = 0; j < state.database.content.sections[i].items.length; j++){
-                        if(state.database.content.sections[i].items[j].id == code){
-                            state.database.content.sections[i].items[j].available = true;
-                            state.database.content.sections[i].unread++;
-                        }
-                    }
-                }
-                break;
-            case "bookmark":
-                var code = data.item;
-                var value = data.value;
-                for(var i = 0; i < state.database.content.sections.length; i++){
-                    for(var j = 0; j < state.database.content.sections[i].items.length; j++){
-                        if(state.database.content.sections[i].items[j].id == code){
-                            state.database.content.sections[i].items[j].bookmark = value;
-                        }
-                    }
-                }
-                break;
-            case "debugReset":
-                actions.loadDatabase();
-                break;
-            case "debugAddAll":
-                for(var i = 0; i < state.database.content.sections.length; i++){
-                    for(var j = 0; j < state.database.content.sections[i].items.length; j++){
-                        state.database.content.sections[i].items[j].available = true;
-                        state.database.content.sections[i].unread++;
-                    }
-                }
-                break;
-            case "debugAddSection":
-                var code = data;
-                for(var i = 0; i < state.database.content.sections.length; i++){
-                    if(state.database.content.sections[i].id == code){
-                        for(var j = 0; j < state.database.content.sections[i].items.length; j++){
-                            state.database.content.sections[i].items[j].available = true;
-                            state.database.content.sections[i].unread++;
-                        }
-                    }
-                }
-                break;
-            case "updateFilter":
-                var filter = data;
-                state.database.settings.onlyUnread = filter;
-                break;
+            });
+        });
+        break;
+      case "restoreDatabase":
+        state.database = data.database;
+        state.content = data.content;
+        state.loadingDB = false;
+        state.databaseReady = true;
+        actions.loadComplete();
+        break;
+      case "loadComplete":
+        state.loadingDB = false;
+        state.databaseReady = true;
+        break;
+      case "readItem":
+        var code = data;
+        for(var i = 0; i < state.database.content.sections.length; i++){
+          for(var j = 0; j < state.database.content.sections[i].items.length; j++){
+            if(state.database.content.sections[i].items[j].id == code){
+              if(!state.database.content.sections[i].items[j].read){
+                state.database.content.sections[i].unread--; 
+              }
+              state.database.content.sections[i].items[j].read = true;
+            }
+          }
         }
-    };
-    plux.createStore("shared", myActionHandler, { "databaseReady": false });    
+        break;
+      case "markSectionRead":
+        var code = data;
+        for(var i = 0; i < state.database.content.sections.length; i++){
+          if(state.database.content.sections[i].id == code){
+            for(var j = 0; j < state.database.content.sections[i].items.length; j++){
+              state.database.content.sections[i].items[j].read = true;
+              state.database.content.sections[i].unread--;
+            }
+          }
+        }
+        break;
+      case "addItem":
+        var code = data;
+        for(var i = 0; i < state.database.content.sections.length; i++){
+          for(var j = 0; j < state.database.content.sections[i].items.length; j++){
+            if(state.database.content.sections[i].items[j].id == code){
+              state.database.content.sections[i].items[j].available = true;
+              state.database.content.sections[i].unread++;
+            }
+          }
+        }
+        break;
+      case "bookmark":
+        var code = data.item;
+        var value = data.value;
+        for(var i = 0; i < state.database.content.sections.length; i++){
+          for(var j = 0; j < state.database.content.sections[i].items.length; j++){
+            if(state.database.content.sections[i].items[j].id == code){
+              state.database.content.sections[i].items[j].bookmark = value;
+            }
+          }
+        }
+        break;
+      case "debugReset":
+        actions.loadDatabase();
+        break;
+      case "debugAddAll":
+        for(var i = 0; i < state.database.content.sections.length; i++){
+          for(var j = 0; j < state.database.content.sections[i].items.length; j++){
+            state.database.content.sections[i].items[j].available = true;
+            state.database.content.sections[i].unread++;
+          }
+        }
+        break;
+      case "debugAddSection":
+        var code = data;
+        for(var i = 0; i < state.database.content.sections.length; i++){
+          if(state.database.content.sections[i].id == code){
+            for(var j = 0; j < state.database.content.sections[i].items.length; j++){
+              state.database.content.sections[i].items[j].available = true;
+              state.database.content.sections[i].unread++;
+            }
+          }
+        }
+        break;
+      case "updateFilter":
+        var filter = data;
+        state.database.settings.onlyUnread = filter;
+        break;
+    }
+  };
+  plux.createStore("shared", myActionHandler, { "databaseReady": false });    
 })();
